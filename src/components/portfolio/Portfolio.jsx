e67de020ed9a56f00b347c8c4e2e5ff4e84f2b79
Eslam358 <you@example.com>
2024-05-25 07:05:49 +0300

    changes

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,6 @@
 import "./portfolio.scss";
 import { useEffect, useState, useContext } from "react";
 
-
 import PortfolioList from "../portfolio/portoliolist";
 import { maincontext_por } from "../../context/portfolio";
 
@@ -23,8 +22,7 @@ const Portfolio = () => {
       .then((data) => {
         setmyDataGet(data);
       })
-      .catch((er) => {
-      });
+      .catch((er) => {});
     // .......................................................
     fetch(
       "https://api.netlify.com/api/v1/sites?access_token=wPE2DAxjc_gtwB-pDK2HsIOm63NtxWL9O8WPawEsjH8"
@@ -36,99 +34,93 @@ const Portfolio = () => {
 
         return data;
       })
-      .catch((er) => {
-      });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+      .catch((er) => {});
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const CardPortfolio = (
     <>
       {myData.map((data, index) => (
-          <div key={data.id} className="card">
-            <div className="img">
-              <img src={data.screenshot_url} alt="img" />
+        <div key={data.id} className="card">
+          <div className="img">
+            <img src={data.screenshot_url} alt="img" />
+          </div>
+          <div className="title">
+            {" "}
+            {data.build_settings.repo_path.split("/")[1]}
+          </div>
+          <div className="button">
+            <div className="show">
+              <a target="_blank" href={data.build_settings.repo_url}>
+                <button>Repo</button>
+              </a>
             </div>
-            <div className="title">
-              {" "}
-              {data.build_settings.repo_path.split("/")[1]}
-            </div>
-            <div className="button">
-              <div className="show">
-                <button href={data.build_settings.repo_url} target="_blank">
-                  Repo
-                </button>
-              </div>
-              <div className="gethup">
-                <button   href={`https://${data.default_domain}`} target="_blank">
-                  show
-                </button>
-              </div>
+            <div className="gethup">
+              <a target="_blank" href={`https://${data.default_domain}`}>
+                <button target="_blank">show</button>
+              </a>
             </div>
           </div>
-        ))}
+        </div>
+      ))}
     </>
   );
   const CardPortfolioGet = (
-    
     <div className="container">
       {CardPortfolio}
-        {myDataGet
-          .filter((a, index) => !myDataArr.includes(a.name))
-          .map((data, index) =>  (
-            <div key={data.id} className="card">
-              <div className="img">
-                <img src={
-                      index % 2 === 0
-                        ? "https://d33wubrfki0l68.cloudfront.net/65655332d77d4c00082e960f/screenshot_2023-11-28-02-41-29-0000.png"
-                        : "https://d33wubrfki0l68.cloudfront.net/649d9927d77a80195522867b/screenshot_2023-06-29-14-46-17-0000.png"
-                    } alt="img" />
-              </div>
-              <div className="title">
-              {data.name}
-              </div>
-              <div className="button">
-                <div className="show">
-                  <button   href={data.html_url} target="_blank">
-                    Repo
-                  </button>
-                </div>
-                <div className="gethup">
-                  <button   href={`https://${data.default_domain}`} target="_blank">
-                    show
-                  </button>
-                </div>
-              </div>
-            </div>
-          ))}
-  
-  </div>
-  );
-  const CardPortfolioArr = (
-    <div className="container">
-  
-        {portfolio.map((data, index) => (
+      {myDataGet
+        .filter((a, index) => !myDataArr.includes(a.name))
+        .map((data, index) => (
           <div key={data.id} className="card">
             <div className="img">
-              <img src={data.img } alt="img" />
+              <img
+                src={
+                  index % 2 === 0
+                    ? "https://d33wubrfki0l68.cloudfront.net/65655332d77d4c00082e960f/screenshot_2023-11-28-02-41-29-0000.png"
+                    : "https://d33wubrfki0l68.cloudfront.net/649d9927d77a80195522867b/screenshot_2023-06-29-14-46-17-0000.png"
+                }
+                alt="img"
+              />
             </div>
-            <div className="title">
-            {data.title}
-            </div>
+            <div className="title">{data.name}</div>
             <div className="button">
               <div className="show">
-                <button   href="#" target="_blank">
-                  Repo
-                </button>
+                <a href={data.html_url} target="_blank">
+                  <button>Repo</button>
+                </a>
               </div>
               <div className="gethup">
-                <button   href="#" target="_blank">
-                  show
-                </button>
+                <a href={`https://${data.default_domain}`} target="_blank">
+                  <button>show</button>
+                </a>
               </div>
             </div>
           </div>
         ))}
-    
+    </div>
+  );
+  const CardPortfolioArr = (
+    <div className="container">
+      {portfolio.map((data, index) => (
+        <div key={data.id} className="card">
+          <div className="img">
+            <img src={data.img} alt="img" />
+          </div>
+          <div className="title">{data.title}</div>
+          <div className="button">
+            <div className="show">
+              <button href="#" target="_blank">
+                Repo
+              </button>
+            </div>
+            <div className="gethup">
+              <button href="#" target="_blank">
+                show
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 
@@ -140,9 +132,8 @@ const Portfolio = () => {
       <div className="por">
         <PortfolioList />
       </div>
-    
 
-      {selected === "Netlify" ? CardPortfolioGet: CardPortfolioArr}
+      {selected === "Netlify" ? CardPortfolioGet : CardPortfolioArr}
     </div>
   );
 };
